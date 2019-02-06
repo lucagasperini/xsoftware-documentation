e8784dd63e40464a0e6e0cd03549eb1c7afac7b5
Luca Gasperini <info@xsoftware.it>
2019-02-06 07:38:05 +0100

    Removed old functions
    Signed-off-by: Luca Gasperini <info@xsoftware.it>

--- xsoftware-documentation.php
+++ xsoftware-documentation.php
@@ -315,51 +315,6 @@ class xs_documentation_plugin
                 ]);
 
         }
-        
-        function download_docs($file)
-        {
-                if (file_exists($file)) {
-                        ob_end_clean();
-                        ob_start();
-                        header('Content-Description: File Transfer');
-                        header('Content-Type: application/octet-stream');
-                        header('Content-Disposition: attachment; filename="'.basename($file).'"');
-                        header('Expires: 0');
-                        header('Cache-Control: must-revalidate');
-                        header('Pragma: public');
-                        header('Content-Length: ' . filesize($file));
-                        readfile($file);
-                        exit;
-                }
-        }
-        
-        function get_products_name()
-        {
-                $conn = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
-
-                if (mysqli_connect_error()) {
-                        die("Connection to database failed: " . mysqli_connect_error());
-                }
-                if(is_resource($conn)) { 
-                        $conn->query($conn, "SET NAMES 'utf8'"); 
-                        $conn->query($conn, "SET CHARACTER SET 'utf8'"); 
-                } 
-                $offset = array();
-                $sql = "SELECT name, title FROM xs_products WHERE lang='en_GB'"; //FIXME: FORCE LANG EN
-                $result = $conn->query($sql);
-                if (!$result) {
-                        echo "Could not run query: SQL_ERROR -> " . $conn->error . " SQL_QUERY -> " . $sql;
-                        exit;
-                }
-                if ($result->num_rows > 0) {
-                        while ($row = $result->fetch_assoc()) {
-                                $offset[$row['name']] = $row['title'];
-                        }
-                }
-                $result->close();
-                return $offset;
-        }
-        
 }
 
 $documentation_plugin = new xs_documentation_plugin;
