38348233734f68d78e9aed2ab392e0d2ec111e0f
Luca Gasperini <info@xsoftware.it>
2019-07-01 08:01:14 +0200

    Added comments on template
    Signed-off-by: Luca Gasperini <info@xsoftware.it>

--- template/archive.php
+++ template/archive.php
@@ -1,75 +1,83 @@
 <?php
         if(!defined("ABSPATH")) die;
 
-        /* Load the template css */
+        /* Load the documention template style */
         wp_enqueue_style('xs_documentation_style', plugins_url('template.min.css', __FILE__));
 
         get_header();
 
+        /* Get the options from documention plugin */
         $option = get_option('xs_options_docs');
-        $option = $option['categories'];
 
-        $layout = get_theme_mod('page_layout');
-
-        echo '<div id="primary" class="archive_content_area '.$layout.'">';
-
-        echo '<main id="main" class="post-wrap" role="main">';
+        /* Print primary and main elements */
+        echo '<div id="primary" class="archive_content_area">
+        <main id="main" class="post-wrap" role="main">';
 
         if ( have_posts() ) {
+                /* Print the header */
                 echo '<header class="page-header">';
                 the_archive_title( '<h3 class="archive-title">', '</h3>' );
                 the_archive_description( '<div class="taxonomy-description">', '</div>' );
                 echo '</header>';
 
+                /* Get all post in $html_list */
                 $html_list = array();
                 while ( have_posts() ) {
                         the_post();
+                        /* Get the post id */
                         $id = get_the_ID();
-                        $product = get_post_meta( $id, 'xs_documentation_category', true );
+                        /* Get the post category */
+                        $category = get_post_meta( $id, 'xs_documentation_category', true );
+                        /* Get the post permalink at 'permalink' */
                         $values['permalink'] = get_permalink($id);
+                        /* Get the post title at 'title' */
                         $values['title'] = get_the_title($id);
-                        $html_list[$product][] = $values;
+                        /* Create the matrix by categories with all important values */
+                        $html_list[$category][] = $values;
                 }
 
-                echo '<div class="posts-layout">';
-                foreach($html_list as $product => $docs) {
+                /* Print the matrix */
+                foreach($html_list as $cat => $docs) {
+                        /* Get the current category */
+                        $current = $option['categories'][$cat];
+                        /* Print the treeview */
                         echo '<ul class="css-treeview">';
-                        echo '<div class="c">';
+                        /* Print the category image */
                         xs_framework::create_image([
-                                'src' => $option[$product]['img'],
-                                'alt' => $option[$product]['name'],
+                                'src' => $current['img'],
+                                'alt' => $current['name'],
                                 'height' => 150,
                                 'width' => 150,
                                 'echo' => TRUE
                         ]);
-                        echo '<div class="c">';
-                        echo '<label>'.$option[$product]['name'].'</label>';
-                        echo '<p>'.$option[$product]['descr'].'</p>';
-                        echo '</div></div>';
+                        /* Print the title of the category */
+                        echo '<label>'.$current['name'].'</label>';
+                        /* Print the description of the category */
+                        echo '<p>'.$current['descr'].'</p>';
+                        /* Print the sub array of documentations */
                         foreach($docs as $single) {
+                                /* Print the row */
                                 echo '<li><div class="row">';
+                                /* Print the link on title*/
                                 echo '<a href="'.$single['permalink'].'">'.$single['title'].'</a>';
+                                /* Print the download link */
                                 echo '<a class="download-link" href="'.$single['permalink'].'?download">';
+                                /* Print the font-awesome icon for download */
                                 echo '<i class="fas fa-file-download"></i>';
+                                /* Close download link */
                                 echo '</a>';
+                                /* Close the row */
                                 echo '</div></li>';
                         }
+                        /* Close the treeview */
                         echo '</ul>';
                 }
-                echo '</div>';
 
-                the_posts_pagination( array(
-                        'mid_size'  => 1,
-                ) );
         } else {
                 get_template_part( 'content', 'none' );
         }
-
+        /* Close primary and main elements */
         echo '</main></div>';
-
-        if ( $layout !== 'fullscreen' )
-                get_sidebar();
-
 
         get_footer();
 ?>
--- template/single.php
+++ template/single.php
@@ -1,46 +1,58 @@
 <?php
         if(!defined("ABSPATH")) die;
 
+        /* If is present the download on URL query */
         if(isset($_GET['download'])) {
+                /* Get the post id */
                 $id = get_the_ID();
-                $post = get_post($id);
+                /* Fetch by post query the post_content */
+                $content = get_post_field('post_content', $id);
+                /* Fetch by post query the post_content */
+                $title = get_post_field('post_title', $id);
 
+                /* Print the header information to download the file */
                 header('Content-Description: File Transfer');
                 header('Content-Type: application/octet-stream');
-                header('Content-Disposition: attachment; filename="'.$post->post_title.'.txt"');
+                header('Content-Disposition: attachment; filename="'.$title.'.txt"');
                 header('Expires: 0');
                 header('Cache-Control: must-revalidate');
                 header('Pragma: public');
-                header('Content-Length: ' . strlen($post->post_content));
-                echo $post->post_content;
+                header('Content-Length: ' . strlen($content));
+                /* Print the RST content to download */
+                echo $content;
+                /* Exit from the script */
                 exit;
         }
 
+        /* Load the documention template style */
         wp_enqueue_style('xs_documentation_style', plugins_url('template.min.css', __FILE__));
 
         get_header();
-        $layout = get_theme_mod('page_layout');
 
-        echo '<div id="primary" class="single_content_area '.$layout.'">';
+        /* Print primary and main elements */
+        echo '<div id="primary" class="archive_content_area">
+        <main id="main" class="post-wrap" role="main">';
 
-        echo '<main id="main" class="post-wrap" role="main">';
+        /* Post loop */
+        /* TODO:It's needed? */
         while ( have_posts() ) {
                 the_post();
+                /* Get the post id */
                 $id = get_the_ID();
 
+                /* Print the title of the documentation */
                 echo '<h1>'.get_the_title($id).'</h1>';
 
+                /* Print the parsed HTML of the documentation*/
                 echo get_post_meta($id, 'xs_documentation_html', true);
 
-                // If comments are open or we have at least one comment, load up the comment template
+                /* If comments are open or it has at least one comment, load the comment template */
                 if ( comments_open() || get_comments_number() )
                         comments_template();
         }
 
+        /* Close primary and main elements */
         echo '</main></div>';
-
-        if ( $layout !== 'fullscreen' )
-                get_sidebar();
 
         get_footer();
 ?>
